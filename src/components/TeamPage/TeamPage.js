4833ceff528d20002fd65d5b093a46ba76bbcf14
dustnhgstrm <77700279+dustnhgstrm@users.noreply.github.com>
2021-10-16 21:02:30 -0500

    changed process.env.NODE_ENV to process.env.REACT_APP_AXIOS in three locations

--- src/components/TeamPage/TeamPage.js
+++ src/components/TeamPage/TeamPage.js
@@ -12,7 +12,7 @@ import { AuthContext } from "../../context/AuthContext";
 
 function TeamPage(props) {
   const baseURL =
-    process.env.NODE_ENV === //react already has this node env variable established.
+    process.env.REACT_APP_AXIOS === //react already has this node env variable established.
     "development"
       ? "http://localhost:8080/api"
       : "/api";
